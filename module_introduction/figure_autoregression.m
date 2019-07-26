ec17fe52e359bc4f5043cb8a2acf94353a7a3add
Ashley Coenen <acoenen3@gatech.edu>
2019-07-26 16:17:28 -0400

    modify figure 1

--- module_introduction/figure_autoregression.m
+++ module_introduction/figure_autoregression.m
@@ -6,20 +6,22 @@ end
 
 fs = 8;
 fig = figure();
+nrow = 3;
+ncol = 2;
 
-subplot(2,2,1);
+subplot(nrow,ncol,1);
 plot(X);
 xlabel('time');
-ylabel('abundance');
+ylabel('X');
 title('Independent random walks');
 set(gca,'FontSize',fs);
 
-subplot(2,2,2);
+subplot(nrow,ncol,2);
 plot_correlation(rho_X,pval_X);
 title(sprintf('Correlations among\nindependent random walks'));
 set(gca,'FontSize',fs);
 
-subplot(2,2,3);
+ax = subplot(nrow,ncol,3);
 plot(Xresidual);
 tmpy = ylim;
 ylim([-1 +1]*max(abs(tmpy)));
@@ -27,12 +29,15 @@ xlabel('time');
 ylabel('X_{residual}');
 title(sprintf('Residuals of\nindependent random walks'));
 set(gca,'FontSize',fs);
+lg = legend(string(1:N),'FontSize',6,'Location','eastoutside','NumColumns',3);
+lg.Title.String = 'timeseries #';
+lg.Position = [ax.Position(1) ax.Position(2)-0.15 ax.Position(3) 0.05];
 
-subplot(2,2,4);
+subplot(nrow,ncol,4);
 plot_correlation(rho_Xresidual,pval_Xresidual);
 title(sprintf('Correlations among\nresidual timeseries'));
 set(gca,'FontSize',fs);
 
 fig.Units = 'inches';
-fig.Position(3:4) = [6 4];
+fig.Position(3:4) = [6 6];
 print('figure_autoregression','-djpeg','-r300');
